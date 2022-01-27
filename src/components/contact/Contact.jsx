a10cf31ed4e40fb902f7a0a9791b8eff00fd27b1
Charan Gautam <gautamsaicharan@yahoo.com>
2022-01-26 22:24:36 -0700

    updated resume

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,7 +12,7 @@ import github from '../../img/github.svg';
 import instagram from '../../img/instagram.svg'
 import linkedin from '../../img/linkedin.svg'
 // resume
-import resume from '../../files/charan_gautam_resume.pdf';
+import resume from '../../files/Charan-Gautam-Resume.pdf';
 // EmailJS
 import emailjs from '@emailjs/browser';
 
